58fe89a3a9135dfe7dd54ddc712a5c2bc98759d7
ikura-salmon <eng.start.info@gmail.com>
2021-03-17 19:19:15 +0900

    Updating sample data.

--- postgresql/lesson01/initdb/001_create_divisions.sql
+++ postgresql/lesson01/initdb/001_create_divisions.sql
@@ -1,6 +1,6 @@
 CREATE TABLE divisions (
-    id serial primary key,
-    name varchar(255)
+    id SERIAL primary key,
+    name VARCHAR(255) NOT NULL
 );
 
 COMMENT ON TABLE divisions IS '部署テーブル';
@@ -8,7 +8,7 @@ COMMENT ON COLUMN divisions.id IS '部署ID';
 COMMENT ON COLUMN divisions.name IS '部署名';
 
 INSERT INTO divisions (id, name) VALUES
-(DEFAULT, '管理部'),
-(DEFAULT, '開発部'),
-(DEFAULT, '営業部'),
-(DEFAULT, '窓際部');
+(1, '管理部'),
+(2, '開発部'),
+(3, '営業部'),
+(4, '窓際部');
---
+++ postgresql/lesson01/initdb/003_create_ranks.sql
@@ -0,0 +1,30 @@
+CREATE TABLE ranks (
+    id SERIAL primary key,
+    name VARCHAR(255) NOT NULL,
+    salary INTEGER NOT NULL
+);
+
+COMMENT ON TABLE ranks IS '職位テーブル';
+COMMENT ON COLUMN ranks.id IS '職位ID';
+COMMENT ON COLUMN ranks.name IS '職位名';
+COMMENT ON COLUMN ranks.salary IS '給与';
+
+INSERT INTO ranks (id, name, salary) VALUES
+(1, 'A', 300),
+(2, 'B', 400),
+(3, 'C', 500),
+(4, 'D', 600),
+(5, 'E', 700),
+(6, 'F', 800),
+(7, 'G', 900),
+(8, 'H', 1000),
+(9, 'I', 1100),
+(10, 'J', 1200),
+(11, 'K', 1300),
+(12, 'L', 1400),
+(13, 'M', 1500),
+(14, 'N', 1600),
+(15, 'O', 1700),
+(16, 'P', 1800),
+(17, 'Q', 1900),
+(18, 'R', 2000);
